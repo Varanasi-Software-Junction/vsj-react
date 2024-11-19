a446e34682f250f3a00c4226ba9d8c37d96d8ecf
champaksworldcreate <champaksworld@gmail.com>
2024-11-19 18:53:50 +0530

    ok

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import './NavigationBar.css';
+import React from "react";
+import "./NavigationBar.css";
 
-function NavigationBar() {
+const NavigationBar = () => {
   return (
-    <nav className="nav-bar">
-      <ul>
-        <li>Home</li>
-        <li>Places to Visit</li>
-        <li>Plan Your Trip</li>
-        <li>Transport</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <div className="navbar-container">
+      <div className="logo">Logo</div>
+      <nav className="menu">
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
   );
-}
+};
 
 export default NavigationBar;
